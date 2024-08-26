bfa14046a9d27f34e3d1742c83e11a3569f1a12f
Ivan_Koch <ivankoch28@gmail.com>
2024-08-26 20:47:36 -0300

    probando

--- testing.py
+++ testing.py
@@ -1,8 +1,18 @@
 testestest = 0
+import os
+clear = lambda: os.system('cls')
+from threading import Timer
+booolean = False
+def testtt():
+    print("holaaa")
+    global booolean 
+    booolean = True
+i=0
 
-def holhola():
-    testestest+= 1
-
-holhola()
-
-testtt =123
+while i < 2234556:
+    r = Timer(2.0, testtt)
+    if booolean == False:
+        r.start()
+    
+    
+    clear()
